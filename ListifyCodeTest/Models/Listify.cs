1f7e4f543658929a117cf58b458fc9346abbb52e
Patrick Watson <pwatson1517@gmail.com>
2020-01-25 09:36:48 -0500

    Added appropriate functionality to the listify model class.

--- ListifyCodeTest/Models/Listify.cs
+++ ListifyCodeTest/Models/Listify.cs
@@ -8,10 +8,62 @@ namespace ListifyCodeTest.Models
 {
     public class Listify : IList<int>
     {
+        public int Min { get; set; }
+        public int Max { get; set; }
+        public int IndexedValue { get; set; }
+        public IEnumerable<int> Range { get; set; }
+
         public int this[int index]
         {
-            get => throw new NotImplementedException();
+            get => getIndex(index);
             set => throw new NotImplementedException();
+        }
+
+        public Listify(int min, int max)
+        {
+            //Making an assumption that we are using ints since that is what Enumerable.Range() returns.
+            //TODO: what to do if they send just like..a massive number?
+            //TODO: Should probably take in strings and just try to parse them. Then I can toss out a format exception if things are not strings.
+            if (max < min)
+            {
+                //This should actually be handled by Enumerable.
+                //throw new ArgumentOutOfRangeException("The begin must be lower than the end.");
+            }
+
+            this.Min = min;
+            this.Max = max;
+
+            this.Range = Enumerable.Range(min, max);
+        }
+
+        private int getIndex(int index)
+        {
+            var returnValue = 0;
+
+            var rangeMin = 0;
+            var rangeMax = this.Max - this.Min;
+
+            if (index < rangeMin || index > rangeMax)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            var indexCount = 0;
+
+            foreach (var value in this.Range)
+            {
+                if (indexCount == index)
+                {
+                    returnValue = value;
+                    break;
+                }
+
+                indexCount++;
+            }
+
+            //TODO: Check?
+            this.IndexedValue = returnValue;
+            return returnValue;
         }
 
         public int Count => throw new NotImplementedException();
