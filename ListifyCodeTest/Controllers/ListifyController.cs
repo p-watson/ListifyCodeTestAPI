b60a5b3c78e8b5998c5aa80dc68426945ac0946f
Patrick Watson <pwatson1517@gmail.com>
2020-01-25 09:38:41 -0500

    Added rudementary functionality to ListifyController class

--- ListifyCodeTest/Controllers/ListifyController.cs
+++ ListifyCodeTest/Controllers/ListifyController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ListifyCodeTest.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +10,25 @@ namespace ListifyCodeTest.Controllers
 {
     public class ListifyController : ApiController
     {
+        [HttpGet]
+        public IHttpActionResult Listify(int begin, int end, int index)
+        {
+            try
+            {
+                var list = new Listify(begin, end);
+                var indexedValue = list[index];
+
+                return Json(list);
+            }
+            catch (IndexOutOfRangeException indexException)
+            {
+                return Json("The index parameter was out of bounds.");
+            }
+            catch (ArgumentOutOfRangeException argumentException)
+            {
+                return Json("The begin was higher than end, or end was lower than begin.");
+            }
+
+        }
     }
 }
